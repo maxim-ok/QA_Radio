73ee0597d178c63bf83ce1f6f8d76d3e9d338571
Maksim Okunev <max.vic.okunev@gmail.com>
2022-06-19 17:11:01 +0300

    Firts commit radio

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,57 @@
+package ru.netology.domain;
+
+public class Radio {
+
+    private int currentVolume;
+    private int currentRadioStation;
+
+    public void nextStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation++;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation--;
+        } else {
+            currentRadioStation = 9;
+        }
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation < 0) {
+            return;
+        }
+        if (currentRadioStation > 9) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,105 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class RadioTest {
+    Radio radio = new Radio();
+
+    @Test
+    void stationValueInBoards1() {
+        radio.setCurrentRadioStation(5);
+        radio.nextStation();
+        assertEquals(6, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void stationValueInBoards2() {
+        radio.setCurrentRadioStation(5);
+        radio.prevStation();
+        assertEquals(4, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void stationValueIsHighBoard() {
+        radio.setCurrentRadioStation(9);
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void stationValueIsLowBoard() {
+        radio.setCurrentRadioStation(0);
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void stationValueIsAboveHighBoard() {
+        radio.setCurrentRadioStation(9);
+        radio.nextStation();
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void stationValueIsBelowLowBoard() {
+        radio.setCurrentRadioStation(0);
+        radio.prevStation();
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void stationValueIsInBoards1() {
+        radio.setCurrentRadioStation(0);
+        radio.setCurrentRadioStation(-3);
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    void stationValueIsInBoards2() {
+        radio.setCurrentRadioStation(9);
+        radio.setCurrentRadioStation(13);
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+
+
+    @Test
+    void increaseVolumeLeve() {
+        radio.setCurrentVolume(2);
+        radio.increaseVolume();
+        assertEquals(3, radio.getCurrentVolume());
+    }
+
+    @Test
+    void decreaseVolumeLeve() {
+        radio.setCurrentVolume(5);
+        radio.decreaseVolume();
+        assertEquals(4, radio.getCurrentVolume());
+    }
+
+    @Test
+    void maxIncreaseVolumeLeve() {
+        radio.setCurrentVolume(10);
+        radio.increaseVolume();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    void minDecreaseVolumeLeve() {
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+
+
+
+
+
+
+
+
+
+}
